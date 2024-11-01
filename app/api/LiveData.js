df45122f789218a0b62eca247ce1c401071b67f7
Masum <masumappsbymasum@gmail.com>
2024-11-01 19:38:19 +0600

    cache removed

--- app/api/LiveData.js
+++ app/api/LiveData.js
@@ -2,16 +2,40 @@
 import axios from "axios";
 
 export  async function getLive() {
-    const res = await axios.get(`${process.env.BASE_URL}/lives`, { withCredentials: true });
+    const res = await axios.get(`${process.env.BASE_URL}/lives`, 
+      {
+         withCredentials: true,
+         headers: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+     });
     return res.data.lives;
 }
 export async function getConfig(){
-    const res = await axios.get(`${process.env.BASE_URL}/config`, { withCredentials: true });
+    const res = await axios.get(`${process.env.BASE_URL}/config`, , 
+      {
+         withCredentials: true,
+         headers: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+     });
     return res.data;
 }
 
 export async function getChannels(){
-  const res = await axios.get(`${process.env.BASE_URL}/channels`, { withCredentials: true });
+  const res = await axios.get(`${process.env.BASE_URL}/channels`, , 
+    {
+       withCredentials: true,
+       headers: {
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+      },
+   });
   return res.data.channels;
 }
 
